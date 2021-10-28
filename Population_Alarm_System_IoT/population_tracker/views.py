d28ff78e5cf661505ae734f4e4fb41ea696db9dc
Tegveer Singh <tegveer2211@gmail.com>
2021-10-28 02:42:46 -0400

    Added basic url settings and template

--- Population_Alarm_System_IoT/population_tracker/views.py
+++ Population_Alarm_System_IoT/population_tracker/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 
 # Create your views here.
+
+# Home page to show the number of people entering
+def home(request):
+    context = {"population" : 39}
+    return render(request, "population_tracker/home.html", context)
